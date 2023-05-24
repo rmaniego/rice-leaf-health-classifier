e2c926623c967af624bffbb4ed0c2ff98eed3934
Rodney Maniego Jr <rodney.maniegojr@gmail.com>
2023-05-24 19:12:29 +0800

    Update model architecture

--- leaf_health_classifier.py
+++ leaf_health_classifier.py
@@ -1,13 +1,15 @@
 import os
+import cv2
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from sklearn.model_selection import train_test_split
 
 def main():
     """
+        Initialize the Rice Leaf Health Classifier.
     """
     
     # An epoch is the full iteration on a dataset
@@ -16,7 +18,7 @@ def main():
     #   when it is deteriorating.
     # An example value would be 100/200, but must be changed
     #   based on the dataset, HW resources, actual dataset, etc.
-    epochs = 200
+    epochs = 100
     classes = 2 # healthy or infected
     shape = (50, 50, 3) # Target Shape: 50x50 RGB images (JPEG)
     
@@ -24,7 +26,7 @@ def main():
     images, _, labels = preprocess_dataset(dataset_path, shape)
     dataset_size = len(images)
 
-    features1, features2, target1, target2 = train_test_split(images, labels, test_size=0.1, random_state=42)
+    features1, features2, target1, target2 = train_test_split(images, labels, test_size=0.2, random_state=42)
     
     model = load_model_from_file(features1, target1, features2, target2, dataset_size, classes, shape, epochs=epochs, batch_size=32)
     
@@ -43,6 +45,11 @@ def preprocess_dataset(dataset_path, shape):
     labels = datasets["label"].tolist()
     
     augmented_labels = []
+    
+    # Rotations: 90, 180, 270
+    rotations = [
+        cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE
+    ]
 
     images = []
     for label, filename in zip(labels, filenames):
@@ -55,11 +62,18 @@ def preprocess_dataset(dataset_path, shape):
         image = tf.keras.preprocessing.image.img_to_array(image)
         
         # Normalize the BGR pixels (0-1)
-        images.append(image/255.0)
+        image = image/255.0
+        images.append(image)
+        augmented_labels.append(label)
+
+        if filename[0] != "X":
+            for rotation in rotations:
+                images.append(cv2.rotate(image, rotation))
+                augmented_labels.append(label)
 
     # Convert the lists to numpy arrays
     images = np.array(images)
-    labels = np.array(labels)
+    labels = np.array(augmented_labels)
     
     return images, filenames, labels
 
@@ -81,20 +95,28 @@ def load_model_from_file(features1, target1, features2, target2, dataset_size, c
         model = Sequential()
 
         # Add convolutional layers
-        model.add(Conv2D(32, (3, 3), activation="relu", input_shape=shape))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation="relu"))
-        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, kernel_size=(3, 3), activation="relu", input_shape=shape))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        
+        model.add(Conv2D(128, kernel_size=(3, 3), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
         # Flatten the output from the previous layer
         model.add(Flatten())
 
         # Add fully connected layers
         model.add(Dense(64, activation="relu"))
-        model.add(Dense(classes, activation="softmax"))
+
+        model.add(Dense(128, activation="relu"))
+        
+        model.add(Dense(classes, activation="sigmoid"))
 
         # Compile the model
         model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+        
+        print(model.summary())
 
         # Train the model
         model.fit(features1, target1, epochs=epochs, batch_size=batch_size, validation_data=(features2, target2))
